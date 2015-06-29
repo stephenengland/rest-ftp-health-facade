25ad251c3784629a02ff36c1bafafacaa17ec91f
Stephen England <Stephen.England@DealerTrack.com>
2015-06-29 14:16:13 -0500

    More deploy script changes

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,2 +1,6 @@
+
+$ScriptDir = (Split-Path $myinvocation.MyCommand.Path)
+set-location $ScriptDir
+
 "C:\Program Files\nodejs\node.exe" ./windows-service.js --uninstall
 "C:\Program Files\nodejs\node.exe" ./windows-service.js --install
