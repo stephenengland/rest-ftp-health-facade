b4acdf7e882fb34db3296a74736726e5dc18eb64
Stephen England <Stephen.England@DealerTrack.com>
2015-08-19 15:47:24 -0500

    Forgot some files. oops.

---
+++ ftpHistory.js
@@ -0,0 +1,83 @@
+var _ = require('underscore-node'),
+	store = require('./node-store')();
+
+var hasLoadedHistory = false;
+var lastKnownSave = null; 
+var history =  {
+	"id": "history"
+};
+var fileIoLimitInMilliseconds = 5000;
+var maxHistory = 10;
+
+//Save the ftp history. We don't care if we 'miss' a save or two.. this isn't sensitive data. Better not to thrash file io.
+var saveHistory = function () {
+	if (hasLoadedHistory && (!lastKnownSave || (lastKnownSave - new Date()) > fileIoLimitInMilliseconds)) {
+		store.add(history, function (err) {
+			if (err) {
+				console.log(err);
+			}
+		});
+	}
+};
+
+var pushToHistory = function (uri, status) {
+	var changed = false;
+	if (!history[uri]) {
+		history[uri] = [{
+			'status': status,
+			'sinceDate': new Date(),
+			'lastChecked': new Date()
+		}];
+		changed = true;
+	}
+	else {
+		var currentItems = _.sortBy(history[uri], function (historyItem) {
+			return historyItem.date;
+		});
+
+		if (currentItems[currentItems.length - 1].status !== status) {
+			if (history[uri].length > maxHistory) {
+				currentItems.shift();
+			}
+			currentItems.push({
+				'status': status,
+				'sinceDate': new Date(),
+				'lastChecked': new Date()
+			});
+			history[uri] = currentItems;
+			changed = true;
+		}
+		else {
+			currentItems[currentItems.length - 1].lastChecked = new Date();
+		}
+	}
+
+	saveHistory();
+};
+
+store.load("history", function (err, loadedHistory) {
+	if (!err) {
+		history = loadedHistory;
+		history.id = "history";
+	}
+	hasLoadedHistory = true;
+});
+
+module.exports = {
+	"processFtpResponse": function (uri, status) {
+		pushToHistory(uri, status);
+	},
+	"getRecentStatus": function (uri) {
+		if (!history.hasOwnProperty(uri) || history[uri].length === 0) {
+			return {
+				'status': 'unknown',
+				'sinceDate': new Date(),
+				'lastChecked': new Date()
+			};
+		}
+		var currentItems = _.sortBy(history[uri], function (historyItem) {
+			return historyItem.date;
+		});
+		return currentItems[currentItems.length - 1];
+	}
+};
---
+++ tests/ftptests_spec.js
@@ -0,0 +1,70 @@
+var frisby = require('frisby'),
+    path = require('path'),
+    nconf = require('nconf');
+
+nconf.argv().env();
+nconf.file({ file: path.join(__dirname, '..', 'config.json')});
+
+frisby.create('Get FTP Server Status')
+  .get('http://localhost:' + nconf.get('port') + '/ftp.ed.ac.uk')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json; charset=utf-8')
+  .expectJSON({
+    type: 'FTP Server'
+  })
+  .expectJSONTypes({
+    statusMessage: String,
+    type: String
+  })
+  //.inspectJSON()
+.toss();
+
+frisby.create('Get FTP Server Status - Bad Host Name')
+  .get('http://localhost:' + nconf.get('port') + '/ftp.axcvzwererzersdfawerze.com')
+  .expectStatus(500)
+  //.inspectJSON()
+.toss();
+
+frisby.create('Get FTP File')
+  .get('http://localhost:' + nconf.get('port') + '/ftp.ed.ac.uk/INSTRUCTIONS-FOR-USING-THIS-SERVICE')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json; charset=utf-8')
+  .expectJSON({
+    numberOfFiles: 1,
+    type: 'FTP File',
+    fileName: 'INSTRUCTIONS-FOR-USING-THIS-SERVICE'
+  })
+  .expectJSONTypes({
+    numberOfFiles: Number,
+    type: String,
+    fileName: String
+  })
+  //.inspectJSON()
+.toss();
+
+
+frisby.create('Get FTP Path - Invalid Path')
+  .get('http://localhost:' + nconf.get('port') + '/ftp.ed.ac.uk/asdfzerwer-zsdfzsdf-zwerzwer-zwerzwer-zwerzwer')
+  .expectStatus(502)
+  //.inspectJSON()
+.toss();
+
+frisby.create('Get FTP Directory')
+  .get('http://localhost:' + nconf.get('port') + '/ftp.ed.ac.uk/pub')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json; charset=utf-8')
+  .expectJSON({
+    type: 'FTP Directory'
+  })
+  .expectJSONTypes({
+    numberOfFiles: Number,
+    type: String
+  })
+  //.inspectJSON()
+.toss();
+
+frisby.create('Get FTP Directory - Empty')
+  .get('http://localhost:' + nconf.get('port') + '/ftp.ed.ac.uk/edupload')
+  .expectStatus(502)
+  //.inspectJSON()
+.toss();
